0ec6dcd953c947085d769ebd9e0bf7f4d2698c71
Luis Alberto Díaz Díaz <73986926+Luis-omega@users.noreply.github.com>
2023-01-25 15:43:40 -0600

    added module parser

--- src/MeguKin/Parser/Parsers.hs
+++ src/MeguKin/Parser/Parsers.hs
@@ -1,23 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module MeguKin.Parser.Parsers (
-  Token (
-    ModuleToken,
-    ImportToken,
-    AsToken,
-    EqualToken,
-    TypeToken,
-    IdentifierToken,
-    LetToken,
-    LeftParenToken,
-    RightParenToken,
-    LeftArrowToken,
-    RightArrowToken,
-    LambdaStartToken,
-    InToken
-  ),
-  lexer,
-) where
+module MeguKin.Parser.Parsers (topLevel) where
 
 import Control.Applicative (Applicative (pure), (<|>))
 import Control.Monad.Combinators.NonEmpty (sepBy1)
@@ -26,101 +9,102 @@ import Data.Functor (($>), (<$>))
 import Data.Maybe (Maybe (Just))
 import Data.Monoid (Monoid (mempty))
 import Data.String (String)
-import Text.Megaparsec (MonadParsec (lookAhead, takeWhile1P), Parsec, satisfy)
+import Text.Megaparsec (MonadParsec (takeWhile1P), between, satisfy, sepBy)
 import Text.Megaparsec.Char (space1)
-import qualified Text.Megaparsec.Char.Lexer (lexeme, space, symbol)
 import qualified Text.Megaparsec.Char.Lexer as Lexer
-import Prelude (($), (==), (||))
+import Prelude (($), (.), (==), (||))
 
 import MeguKin.Parser.Types (
+  ExportDeclaration (DataClassExport, DataTypeExport),
+  Identifier (Identifier),
   Parser,
-  Token (
-    AsToken,
-    EqualToken,
-    IdentifierToken,
-    ImportToken,
-    InToken,
-    LambdaStartToken,
-    LeftArrowToken,
-    LeftParenToken,
-    LetToken,
-    ModuleToken,
-    RightArrowToken,
-    RightParenToken,
-    TypeToken
-  ),
+  SimpleIdentifier (SimpleIdentifier),
+  TopLevelDefinition (ModuleDeclaration),
  )
 
 consumeSpaces :: Parser ()
-consumeSpaces = Text.Megaparsec.Char.Lexer.space space1 mempty mempty
+consumeSpaces = Lexer.space space1 mempty mempty
 
 consumeNonNewLineSpaces :: Parser ()
 consumeNonNewLineSpaces =
-  Text.Megaparsec.Char.Lexer.space
+  Lexer.space
     (satisfy isSimpleSpace $> ())
     mempty
     mempty
  where
   isSimpleSpace x = x == ' ' || x == '\t'
 
-lexeme :: Parser a -> Parser a
-lexeme = Text.Megaparsec.Char.Lexer.lexeme consumeNonNewLineSpaces
+-- lexeme :: Parser a -> Parser a
+-- lexeme = Lexer.lexeme consumeNonNewLineSpaces
 
 simpleSymbol :: String -> Parser String
-simpleSymbol = Text.Megaparsec.Char.Lexer.symbol consumeNonNewLineSpaces
+simpleSymbol = Lexer.symbol consumeNonNewLineSpaces
 
-symbol :: String -> Token -> Parser Token
-symbol str tok = Text.Megaparsec.Char.Lexer.symbol consumeNonNewLineSpaces str $> tok
+symbol :: String -> Parser ()
+symbol str = Lexer.symbol consumeNonNewLineSpaces str $> ()
 
-module_ :: Parser Token
-module_ = symbol "module" ModuleToken
-import_ :: Parser Token
-import_ = symbol "import" ImportToken
-as_ :: Parser Token
-as_ = symbol "as" AsToken
-equalOperator :: Parser Token
-equalOperator = symbol "=" EqualToken
-typeOperator :: Parser Token
-typeOperator = symbol "::" TypeToken
-lparen :: Parser Token
-lparen = symbol "(" LeftParenToken
-rparen :: Parser Token
-rparen = symbol ")" RightArrowToken
-let_ :: Parser Token
-let_ = symbol "let" LetToken
-leftArrow :: Parser Token
-leftArrow = symbol "<-" LeftArrowToken
-rightArrow :: Parser Token
-rightArrow = symbol "->" RightArrowToken
-in_ :: Parser Token
-in_ = symbol "in" InToken
-lambdaStart :: Parser Token
-lambdaStart = symbol "\\" LambdaStartToken
+module_ :: Parser ()
+module_ = symbol "module"
+where_ :: Parser ()
+where_ = symbol "where"
+lparen :: Parser ()
+lparen = symbol "("
+rparen :: Parser ()
+rparen = symbol ")"
+comma :: Parser ()
+comma = symbol ","
 
-simpleIdentifier :: Parser String
-simpleIdentifier = takeWhile1P (Just "IdentifierCharacter") isLetter
+-- import_ :: Parser ()
+-- import_ = symbol "import"
+-- as_ :: Parser ()
+-- as_ = symbol "as"
+-- equalOperator :: Parser ()
+-- equalOperator = symbol "="
+-- typeOperator :: Parser ()
+-- typeOperator = symbol "::"
+-- let_ :: Parser ()
+-- let_ = symbol "let"
+-- leftArrow :: Parser ()
+-- leftArrow = symbol "<-"
+-- rightArrow :: Parser ()
+-- rightArrow = symbol "->"
+-- in_ :: Parser ()
+-- in_ = symbol "in"
+-- lambdaStart :: Parser ()
+-- lambdaStart = symbol "\\"
+-- class_ :: Parser ()
+-- class_ = symbol "class"
 
-longIdentifier :: Parser Token
+simpleIdentifier :: Parser SimpleIdentifier
+simpleIdentifier = SimpleIdentifier <$> takeWhile1P (Just "IdentifierCharacter") isLetter
+
+longIdentifier :: Parser Identifier
 longIdentifier = do
   value <- sepBy1 simpleIdentifier (simpleSymbol ".")
-  pure $ IdentifierToken $ Identifier $ value
+  pure . Identifier $ value
 
-data IndentationState = AtStart | AtParen | AtLet | AtEq
+topLevel :: Parser TopLevelDefinition
+topLevel = moduleDeclaration
 
-lexNonIndentation :: Parser [Token]
-lexNonIndentation =
-  pure
-    <$> ( module_
-            <|> import_
-            <|> as_
-            <|> typeOperator
-            <|> lparen
-            <|> rparen
-            <|> leftArrow
-            <|> rightArrow
-            <|> lambdaStart
-            <|> longIdentifier
-        )
+moduleDeclaration :: Parser TopLevelDefinition
+moduleDeclaration = do
+  _ <- Lexer.nonIndented consumeSpaces module_
+  moduleName <- longIdentifier
+  exports <- between lparen rparen (sepBy (dataTypeExport <|> classExport) comma)
+  _ <- where_
+  pure $ ModuleDeclaration moduleName exports
+
+dataTypeExport :: Parser ExportDeclaration
+dataTypeExport = do
+  typeName <- simpleIdentifier
+  exportedValues <- between lparen rparen (sepBy simpleIdentifier comma)
+  pure $ DataTypeExport typeName exportedValues
+
+classExport :: Parser ExportDeclaration
+classExport = do
+  className <- simpleIdentifier
+  exportedValues <- between lparen rparen (sepBy simpleIdentifier comma)
+  pure $ DataClassExport className exportedValues
 
 -- parseLet :: Parser SugaredExpression
 -- parseLet = Lexer.indentBlock consumeSpaces p *> in_
@@ -128,13 +112,3 @@ lexNonIndentation =
 --   p = do
 --     _ <- let_
 --     pure $ Lexer.IndentSome Nothing joinBindings parsePatternMatch
-
-lexIndentation :: Parser Token -> Parser [Token]
-lexIndentation p = do
-  lookAhead p
-
-lexer :: Parser [Token]
-lexer = do
-  consumeSpaces
-  value <- lexNonIndentation
-  pure value
--- src/MeguKin/Parser/Types.hs
+++ src/MeguKin/Parser/Types.hs
@@ -1,21 +1,9 @@
 module MeguKin.Parser.Types (
-  Token (
-    ModuleToken,
-    ImportToken,
-    AsToken,
-    EqualToken,
-    TypeToken,
-    IdentifierToken,
-    LetToken,
-    LeftParenToken,
-    RightParenToken,
-    LeftArrowToken,
-    RightArrowToken,
-    LambdaStartToken,
-    InToken
-  ),
   Parser,
   Identifier (Identifier),
+  SimpleIdentifier (SimpleIdentifier),
+  TopLevelDefinition (ModuleDeclaration),
+  ExportDeclaration (DataTypeExport, DataClassExport),
 ) where
 
 import Data.List.NonEmpty (NonEmpty)
@@ -24,19 +12,14 @@ import Text.Megaparsec (Parsec)
 
 type Parser a = Parsec String String a
 
-newtype Identifier = Identifier (NonEmpty String)
+-- | Do import the constructor! only allowed in Parsers.hs
+newtype SimpleIdentifier = SimpleIdentifier String
 
-data Token
-  = ModuleToken
-  | ImportToken
-  | AsToken
-  | EqualToken
-  | TypeToken
-  | LetToken
-  | LeftParenToken
-  | RightParenToken
-  | LeftArrowToken
-  | RightArrowToken
-  | InToken
-  | LambdaStartToken
-  | IdentifierToken Identifier
+newtype Identifier = Identifier (NonEmpty SimpleIdentifier)
+
+data TopLevelDefinition
+  = ModuleDeclaration Identifier [ExportDeclaration]
+
+data ExportDeclaration
+  = DataTypeExport SimpleIdentifier [SimpleIdentifier]
+  | DataClassExport SimpleIdentifier [SimpleIdentifier]
